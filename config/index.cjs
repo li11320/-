0205ac4b57dfa4a4716da8882361ef86713fc273
gh5678 <148436353+gh5678@users.noreply.github.com>
2024-01-23 16:03:32 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -13,8 +13,8 @@ const USER_CONFIG = {
   // 使用微信测试号：公众号APP_SECRET
   APP_SECRET: '73e53c8ed8b285322929c7d0754ac33f',
 
-  PROVINCE: '陕西省',
-  CITY: '西安市',
+  PROVINCE: '北京市',
+  CITY: '朝阳区',
 
   USERS: [
     {
@@ -29,20 +29,20 @@ const USER_CONFIG = {
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
-          type: '*生日', name: '宝贝', year: '1996', date: '09-09',
+          type: '*生日', name: '锦宝宝', year: '2006', date: '07-17',
         },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
-          type: '生日', name: '李四', year: '1996', date: '09-31',
+          type: '生日', name: '小盆子', year: '2006', date: '06-29',
         },
         {
-          type: '节日', name: '相识纪念日', year: '2020', date: '09-03',
+          type: '节日', name: '相识纪念日', year: '2023', date: '10-03',
         },
       ],
       // 我们在一起已经有xxxx天了的配置
       customizedDateList: [
         // 在一起的日子
-        { keyword: 'love_day', date: '2022-09-08' },
+        { keyword: 'love_day', date: '2023-10-05' },
         // 结婚纪念日
         { keyword: 'marry_day', date: '2022-09-09' },
       ],
