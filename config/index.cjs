6deaaaf0a8cc4ad522dc88885fbbbea44fb4ab92
gh5678 <148436353+gh5678@users.noreply.github.com>
2024-02-26 23:27:47 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -73,7 +73,7 @@ const USER_CONFIG = {
   
   TIAN_API: {
     // 天行API KEY，如果使用天行API则需要填写此项
-    key: 'a165094c39e0030553b693210dac9926',
+    key: '',
     
     /** 天行API相关，需要config中配置 TIAN_API_KEY  */
     // 早安心语, 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
@@ -105,20 +105,20 @@ const USER_CONFIG = {
   /**
    * 接收公众号消息的微信号，如果有多个，需要在[]里用英文逗号间隔
    */
-  USERS: [olDYP6_JER3zlGATHjIX45_-NlJw]
+  USERS: [
     {
       // 想要发送的人的名字
-      name: '锦宝宝~',
+      name: '老婆0',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       // 使用其他通道时，请严格按照各个通道的教程进行填写
       id: '',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       // 使用其他通道时，请填写 config/template-config.cjs 中某个想要使用的模板的id
-      useTemplateId: 'N4Wq8OQ69cztUmQZBm5ytXpKbE877YBBNrref8GqTOY',
+      useTemplateId: 'BLOGOVxegmBvXLPCaohzeBFSl6sPETgq1jd79MK0FyM',
       // 所在省份或城市，也可以不填
-      province: '北京',
+      province: '惠州',
       // 所在城市或县区
-      city: '朝阳区',
+      city: '惠阳',
       // 新历生日, 仅用作获取星座运势, 格式必须
       horoscopeDate: '12-27',
       // 获取什么时候的星座运势，可选：['今日', '明日', '本周', '本月', '今年'], 留空则随机
@@ -145,7 +145,7 @@ const USER_CONFIG = {
       // 专属纪念日/倒数日，如果你在这里填写了纪念日/倒数日，就不会计算CUSTOMIZED_DATE_LIST的日子了, 和CUSTOMIZED_DATE_LIST的配置方法相同，可以往下查看，我这里就不重复写了
       customizedDateList: [
         // 在一起的日子
-        { keyword: 'love_day', date: '2023-10-5' },
+        { keyword: 'love_day', date: '2022-09-08' },
         // 结婚纪念日
         { keyword: 'marry_day', date: '2022-09-09' },
         // 退伍日
@@ -280,3 +280,115 @@ const USER_CONFIG = {
     // 你可以不断按格式往下增加
     // ...
   ],
+
+  /**
+   * 信息配置
+   */
+
+  /** 
+   * 天气相关
+   * 
+   * 可以填 省份或者市区， PROVINCE 的区域必须包含 CITY
+   * 例如： PROVINCE：'广东' CITY：'惠州'
+   * 例如： PROVINCE：'咸阳' CITY：'长武'
+   *
+   * 甚至 PROVINCE 可以不填
+   * 例如： PROVINCE：'' CITY：'长武'
+   * */
+  
+  // 默认所在省份, USERS 中没填的话, 会默认拿这里的省份
+  PROVINCE: '咸阳',
+  // 默认所在城市, USERS 中没填的话, 会默认拿这里的城市
+  CITY: '长武',
+
+  /** 重要节日相关 */
+
+  /**
+   * 重要节日，修改名字为对应需要显示的名字, date 仅填月日即可, 请严格按照示例填写
+   * type必须填！ 只能 “生日” 和 “节日” 二选一!
+   *
+   * --- 阴历配置开始 ---
+   * 如果日期使用阴历，请在 “生日” 或 "节日“ 前添加 * 符号
+   * --- 阴历配置结束 ---
+   *
+   * 生日时，name填写想要展示的名字，你可以填“美丽可爱亲亲老婆”
+   * 节日时，name填写相应展示的节日，你可以填“被搭讪纪念日”
+   *
+   * --- 是否展示周岁信息开始 ---
+   * isShowAge: true 展示岁数, 仅type为生日生效
+   * isShowAge: false 不展示岁数, 仅type为生日生效
+   * 删除isShowAge属性，也会不展示岁数
+   * --- 是否展示周岁信息结束 ---
+   */
+  FESTIVALS: [
+    // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
+    {
+      type: '*生日', name: '老婆', year: '1996', date: '09-09', isShowAge: true,
+    },
+    {
+      type: '节日', name: '结婚纪念日', year: '2020', date: '09-03',
+    },
+    // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
+    {
+      type: '生日', name: '李四', year: '1996', date: '09-31', isShowAge: true,
+    },
+    {
+      type: '节日', name: '被搭讪纪念日', year: '2021', date: '09-01',
+    }
+    // 你可以不断按格式往下增加
+    // ...
+  ],
+
+  /**
+   * 限制重要节日的展示条目, 需要填写数字;
+   * 如果为3, 则仅展示“将要到达” 的3个重要节日提醒，剩下的将被忽略
+   * 如果为0, 则默认展示全部
+   */
+  FESTIVALS_LIMIT: 4,
+
+  /** 日期相关 */
+
+  /** 你现在可以随心增加你认为的所有的需要纪念的日子啦！
+   * keyword是指暴露给测试号的模板字段，填什么就暴露什么, 请注意不要和README的出参表中的字段重复。
+   * 比如：keyword: "love_date" ，在测试号中就是 {{ love_date.DATA }}
+   * */
+  CUSTOMIZED_DATE_LIST: [
+    // 在一起的日子
+    { keyword: 'love_day', date: '2022-09-08' },
+    // 结婚纪念日
+    { keyword: 'marry_day', date: '2022-09-09' },
+    // 退伍日
+    { keyword: 'ex_day', date: '2022-09-10' }
+    // 你可以不断按格式往下增加
+    // ...
+  ],
+  
+  // 默认的课表配置
+  COURSE_SCHEDULE: null,
+
+  /** 插槽 */
+
+  /** 你可以在这里写超多的你想显示的内容了！
+   * keyword是指暴露给测试号的模板字段，填什么就暴露什么, 请注意不要和README的出参表中的字段重复。
+   * 比如：keyword: "lover_prattle" ，在测试号中就是 {{ lover_prattle.DATA }}
+   * */
+  SLOT_LIST: [
+    // 这样配置的话，就会每次发送这句话
+    { keyword: 'encourage_oneself', contents: '你主要的问题在于读书太少而想得太多' },
+    // 这样配置的话，就会每次随机选一句话发送
+    {
+      keyword: 'lover_prattle',
+      contents: [
+        '因为太喜欢你，所以看谁都像是情敌。',
+        '申请成为你爱里的永久居民。',
+        '你很傻，你很笨，可我还是很羡慕你，因为你有我',
+        '遇见你，就好像捡到了100斤的运气'
+      ],
+    }
+    // 你可以不断按格式往下增加
+    // ...
+  ],
+}
+
+module.exports = USER_CONFIG
+
