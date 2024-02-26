d6c2fc1e469e49615125a92754d9289328804d4a
gh5678 <148436353+gh5678@users.noreply.github.com>
2024-02-27 00:30:50 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -114,7 +114,7 @@ const USER_CONFIG = {
       id: 'olDYP6_JER3zlGATHjIX45_-NlJw',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       // 使用其他通道时，请填写 config/template-config.cjs 中某个想要使用的模板的id
-      useTemplateId: 'N4Wq8OQ69cztUmQZBm5ytXpKbE877YBBNrref8GqTOY','JG1ZQ98-XLI1l78wOFK3Ud1ADeHCSuyvRz053LFhJ5A',
+      useTemplateId: 'N4Wq8OQ69cztUmQZBm5ytXpKbE877YBBNrref8GqTOY',
       // 所在省份或城市，也可以不填
       province: '惠州',
       // 所在城市或县区
